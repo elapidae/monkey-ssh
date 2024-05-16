97c2d5757fb336e6bfbb3a9e6e04b2ca9a1f3c3a
elapidae <elapidae@yandex.ru>
2024-05-16 21:02:34 +0400

    run-once -- added ids and temporary files for ps..

--- run-once/main.cpp
+++ run-once/main.cpp
@@ -4,48 +4,56 @@
 #include "vcmdline_parser.h"
 #include "vbyte_buffer.h"
 #include "vlog.h"
+#include "vcat.h"
+#include <stdlib.h>
 
-using namespace std;
+using std::string;
 
 int main( int argc, char ** argv )
 {
-    vlog::set_shared_log( "/tmp/run-once.log", 16*1024, 3 );
-
-    string tmpfile = "/tmp/ps-once.log";
-
     vcmdline_parser args(argc, argv);
-    //auto pidfile = args.take_starts_with( "pidfile=" );
     auto cmd = args.take_starts_with( "cmd=" );
+    auto id = args.safe_starts_with( "id=", "ID" );
+
+    vlog::set_shared_log( "/tmp/run-once-" + id + ".log", 16*1024, 3 );
+
+    std::string tmp_name = "/tmp/run-once-XXXXXX";
+    tmp_name = mktemp( tmp_name.data() );
+    vdeb << "tmp filename:" << tmp_name;
 
     string ps = R"(ps -e --format="cmd"|grep ")" + cmd + '"';
-    ps += " >" + tmpfile;
-    cout << "ps cmd >>> " << ps << endl;
+    ps += " >" + tmp_name;
+    vtrace << "ps cmd >>> " << ps;
     system( ps.c_str() );
 
-    ifstream ifile;
-    ifile.open( tmpfile, ios_base::in );
-    if ( ifile.bad() )
+    auto lines = [tmp_name]
     {
-        cout << "Cannot read file " << tmpfile << ", exit..." << endl;
-        return 1;
-    }
-    string data;
-    data.resize(1000000);
-    auto sz = ifile.readsome( data.data(), data.size() );
-    data.resize(sz);
-    auto list = vbyte_buffer::split(data, '\n');
-    for ( auto line: list )
+        std::ifstream ifile;
+        ifile.open( tmp_name, std::ios_base::in );
+        if ( ifile.bad() )
+        {
+            throw verror << "Cannot read file " << tmpfile;
+        }
+        string data;
+        data.resize(1000000);
+        auto sz = ifile.readsome( data.data(), data.size() );
+        data.resize(sz);
+        auto res = vbyte_buffer::split(data, '\n');
+        system( ("rm " + tmp_name).c_str() );
+        return res;
+    }();
+    for ( auto line: lines )
     {
-        cout << "ps >>> " << line << endl;
+        vtrace << "ps >>> " << line;
         if ( line != cmd ) continue;
 
-        cout << "Found exactly identical command, exiting..." << endl;
+        vdeb << "Found exactly identical command, exiting...";
         return 0;
     }
 
-    cout << "Cmd not found by ps, let's start it..." << endl;
+    vdeb << "Cmd not found by ps, let's start it...";
     auto rcode = system( cmd.c_str() );
-    cout << "system exec returned " << rcode << endl;
+    vdeb << "system exec returned " << rcode;
 
     return 0;
 }
